3cb7e4e911df01f93517652db7348d00016ec2ff
vivekgondaliya <vg13@zips.uakron.edu>
2018-11-21 23:23:44 -0800

    Extracted TableBody - Version Vivek

--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Like from "./commons/like";
 import TableHeader from "./commons/tableHeader";
+import TableBody from "./commons/tableBody";
 
 class MovieTable extends Component {
   //moved this logic out of Movies component,
@@ -26,27 +26,7 @@ class MovieTable extends Component {
           sortColumn={sortColumn}
           onSort={onSort}
         />
-        <tbody>
-          {movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like liked={movie.like} onClick={() => onLike(movie)} />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={() => onDelete(movie)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <TableBody movies={movies} onDelete={onDelete} onLike={onLike} />
       </table>
     );
   }
